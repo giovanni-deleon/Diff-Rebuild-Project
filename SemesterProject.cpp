8235de80a3985c88540713c72dc19c6ea3c73510
Giovanni De Leon <giothermal1234@gmail.com>
2025-03-19 10:00:48 -0500

    Create SemesterProject.cpp

---
+++ SemesterProject.cpp
@@ -0,0 +1,16 @@
+// Name: Giovanni De Leon
+// Desc: Semester Project
+// Main File
+
+#include <iostream>
+#include "FileService.h"
+#include <fstream>
+#include <vector>
+using namespace std;
+
+int main(){
+  FileService fileService;
+  string fileName, fileNameTwo;
+
+  fileService.diff(fileName, fileNameTwo);
+}
