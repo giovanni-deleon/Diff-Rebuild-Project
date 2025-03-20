5a437367a84d88136ea154633bb509192f8ee95a
Giovanni De Leon <giothermal1234@gmail.com>
2025-03-20 08:57:08 -0500

    Create AlteredFileService.h
    Added my personal compare files function.

---
+++ AlteredFileService.h
@@ -0,0 +1,22 @@
+// AUTHOR: Giovanni De Leon
+// DESC: H FileService For Semester Proj.
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class FileService {
+
+public:
+    FileService();// Constructor.
+    vector<int> compareFiles(const vector<string>& fileOneContents, const vector<string>& fileTwoContents);
+    void compareFilesTwo(const vector<string>& fileOneContents, const vector<string>& fileTwoContents);
+
+
+    string removeWhitespace(const string& str);
+
+    bool isLineEmpty(const string& line);
+
+    void diff(string pFileName, string pFileNameTwo);// Takes File Name As Parameter.
+
+};
