95110e1e31fc6e7818e71e10acd3f35d7aec6ad7
Giovanni De Leon <giothermal1234@gmail.com>
2025-04-19 14:04:09 -0500

    Update AlteredFileService.h

--- AlteredFileService.h
+++ AlteredFileService.h
@@ -3,6 +3,7 @@
 
 #include<iostream>
 #include<vector>
+#include <string>
 using namespace std;
 
 class FileService {
@@ -11,6 +12,7 @@ public:
     FileService();// Constructor.
     void compareFilesTwo(const vector<pair<int, string>>& fileOneContents, const vector<pair<int, string>>& fileTwoContents);
 
+    void fancyDiff(int pIndex, int fileOneLineAmount, int fileTwoLineAmount, const vector<pair<int, string>>& fileOneContents, const vector<pair<int, string>>& fileTwoContents);
 
     string removeWhitespace(const string& str);
 
