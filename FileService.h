e167192fc9b6c17da4e1582933f529e2ea1432a5
Giovanni De Leon <giothermal1234@gmail.com>
2025-04-28 20:46:13 -0500

    Update FileService.h
    Works on Linux

--- FileService.h
+++ FileService.h
@@ -1,21 +1,30 @@
-
 // AUTHOR: Giovanni De Leon
 // DESC: H FileService For Semester Proj.
 
+#ifndef FILESERVICE_H
+#define FILESERVICE_H
+
 #include<iostream>
 #include<vector>
+#include <string>
 using namespace std;
 
-class FileService{
+class FileService {
 
-  public:
+public:
     FileService();// Constructor.
-    vector<int> compareFiles(const vector<string>& fileOneContents, const vector<string>& fileTwoContents);
+    void compareFilesTwo(const std::vector<std::pair<int, std::string>>& fileOneContents, const std::vector<std::pair<int, std::string>>& fileTwoContents);
 
-    string removeWhitespace(const string& str);
+    void fancyDiff(int pIndex, int fileOneLineAmount, int fileTwoLineAmount, const std::vector<pair<int, string>>& fileOneContents, const std::vector<pair<int, string>>& fileTwoContents);
 
-    bool isLineEmpty(const string& line);
+    std::string removeWhitespace(const std::string& str);
 
-    void diff(string pFileName, string pFileNameTwo);// Takes File Name As Parameter.
+    bool isLineEmpty(const std::string& line);
+
+    void diff(std::string pFileName, std::string pFileNameTwo);// Takes File Name As Parameter.
     
- };
+    void promptingServ();
+
+};
+
+#endif // FILESERVICE_H
